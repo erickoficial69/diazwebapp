47fe80b91cb8cb9e7d3c4bb64265854b34d51cba
erickoficial69 <erickoficial69@gmail.com>
2021-07-01 00:16:01 -0400

    06-30-2021

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { AppProps } from "next/app";
 import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
-import './index.css'
 import { App_provider } from "../context/wp_context/app_context";
 import Head from 'next/head'
 
@@ -12,6 +11,7 @@ function Myapp({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="shortcut icon" href="/logo.png" />
         <link rel="apple-touch-icon" href="/logo.png"/>
+        <link rel="stylesheet" href="/css/index.css"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
         <meta name="theme-color" content="rgb(10,10,10)" />
       </Head>
